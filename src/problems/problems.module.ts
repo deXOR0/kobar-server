8be12f259ee7e955ac2fe5e0349239384fc9105d
Atyanta Awesa <awesaatyanta.p@gmail.com>
2022-11-05 12:47:51 +0700

    Added problems guard as provider to problems module

--- src/problems/problems.module.ts
+++ src/problems/problems.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { PrismaService } from 'src/prisma.service';
 import { ProblemsController } from './problems.controller';
+import { ProblemsGuard } from './problems.guard';
 import { ProblemsService } from './problems.service';
 
 @Module({
     controllers: [ProblemsController],
-    providers: [ProblemsService, PrismaService],
+    providers: [ProblemsService, PrismaService, ProblemsGuard],
     exports: [ProblemsService],
 })
 export class ProblemsModule {}
