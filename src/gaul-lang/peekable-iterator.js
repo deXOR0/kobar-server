7606651c48f5326aa7315d88eed6d75148e0c345
Atyanta Awesa <awesaatyanta.p@gmail.com>
2022-11-09 13:15:31 +0700

    Fixed gaul-lang import token

--- src/gaul-lang/peekable-iterator.js
+++ src/gaul-lang/peekable-iterator.js
@@ -1,26 +1,32 @@
 export default class PeekableIterator {
-  constructor (string) {
-    this.it = string[Symbol.iterator]();
-    this.#peekNext();
-  }
-
-  #peekNext () {
-    this.peek = this.it.next();
-  }
-
-  [Symbol.iterator] () {
-    return this;
-  }
-
-  next () {
-    const { value, done } = this.peek;
-
-    if (done) {
-      return { value: { current: undefined, next: undefined }, done: true };
+    constructor(string) {
+        this.it = string[Symbol.iterator]();
+        this.#peekNext();
     }
 
-    this.#peekNext();
+    #peekNext() {
+        this.peek = this.it.next();
+    }
 
-    return { value: { current: value, next: this.peek.value }, done: false };
-  }
+    [Symbol.iterator]() {
+        return this;
+    }
+
+    next() {
+        const { value, done } = this.peek;
+
+        if (done) {
+            return {
+                value: { current: undefined, next: undefined },
+                done: true,
+            };
+        }
+
+        this.#peekNext();
+
+        return {
+            value: { current: value, next: this.peek.value },
+            done: false,
+        };
+    }
 }
